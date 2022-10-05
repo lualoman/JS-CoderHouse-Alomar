773cad364f6a04a04260193bdd77e3fb55eea564
Lucia Alomar <alolu7@gmail.com>
2022-10-04 22:21:27 -0300

    Actualizando repo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,33 @@ const NavBar = () => {
         <>
             <div className="navbar bg-base-100">
                 <div className="flex-1">
-                    <Link className="btn btn-light normal-case text-xl" to='/'>TecShop</Link>
+                <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-light btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <CartWidget />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <Link className="justify-between nav-link" style={styles} to='/'>
+                                    Inicio
+                                </Link>
+                            </li>
+                            <li tabIndex={0}></li>
+                            <li><Link className="nav-link" style={styles} to='/Productos'>Productos</Link></li>
+                            <li><Link className="nav-link" style={styles} to='/producto/category'>Categorias
+                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                            </Link>
+                                <ul className="p-2 bg-base-100">
+                                    <li><Link style={styles} to={`/producto/category`}>Accesorios</Link></li>
+                                    <li><Link style={styles} to={`/producto/category`}>Computadoras</Link></li>
+                                    <li><Link style={styles} to={`/producto/category`}>Sonido</Link></li>
+                                </ul>
+                            </li>
+                            <li><Link className="nav-link" style={styles} to='/Contacto'>Contacto</Link></li>
+                        </ul>
+                        </div>
+
                 </div>
                 <div className="flex-none">
                     <div className="dropdown dropdown-end">
@@ -33,20 +59,7 @@ const NavBar = () => {
                         </div>
                     </div>
                     <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-light btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                            <CartWidget />
-                            </div>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li>
-                                <Link className="justify-between nav-link" style={styles} to='/'>
-                                    Inicio
-                                </Link>
-                            </li>
-                            <li><Link className="nav-link" style={styles} to='/Productos'>Productos</Link></li>
-                            <li><Link className="nav-link" style={styles} to='/Contacto'>Contacto</Link></li>
-                        </ul>
+                    <Link className="btn btn-light normal-case text-xl ml-5" to='/'>TecShop</Link>
                     </div>
                 </div>
             </div>
