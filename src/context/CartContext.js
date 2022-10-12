c1654d411d867664b47f13a97df592eda66689d2
Lucia Alomar <alolu7@gmail.com>
2022-10-12 18:28:57 -0300

    Trabajando con CartContext en ACTV 5

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,44 +3,52 @@ import { createContext, useState } from "react";
 export const CartContext = createContext([])
 
 const CartContextProvider = ({ children }) => {
-    
+
     const [cartList, setCartList] = useState([])
 
-    const addToCart = ( item, counter ) => {
+    const addToCart = (item, counter) => {
         console.log(`Hiciste un addToCart de: ${item.name}, cantidad: ${counter}`);
-        setCartList( cartList => cartList.concat(item) )
-        
-        //ALTERNATIVA a setCartList( cartList => cartList.concat(item) ) 
-        //let compra = {...item, quantity:counter}
-        //setCartList([...cartList, compra])
+        //ALTERNATIVA setCartList( cartList => cartList.concat(item) )
+        let compra = { ...item, quantity: counter }
+        setCartList([...cartList, compra])
 
-            if( isInCart (item.id) ){
-                
-            }else{
-                setCartList( [...cartList , item] )
-            }
-
+        if (isInCart(item.id)) {
+            const carritoAux = cartList.map(prod => {
+                if (prod.id === item.id) {
+                    const newProduct = {
+                        ...prod,
+                        cant: prod.cant + cant
+                    }
+                    return newProduct
+                } else {
+                    return prod
+                }
+            })
+            setCartList(carritoAux)
+        } else {
+            setCartList([...cartList, compra])
         }
+    }
 
-        console.log(cartList);
+    //console.log(cartList);
 
     const removeList = () => {
         setCartList.clear()
-        }
+    }
 
     const deleteItem = (id) => {
-        const updateCart = cartList.filter(element => element.id !==id)
+        const updateCart = cartList.filter(element => element.id !== id)
         setCartList(updateCart)
-        }
+    }
 
     const isInCart = (id) => {
         return setCartList.some(element => element.id === id)
-        }
-    
+    }
+
     const context = {
-        cartList, 
-        addToCart, 
-        removeList, 
+        cartList,
+        addToCart,
+        removeList,
         deleteItem
     }
 
