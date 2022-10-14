df157751bd0c26311449e3933ec2eff01a840891
Lucia Alomar <alolu7@gmail.com>
2022-10-14 01:24:36 -0300

    seguir con total de carrito

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -10,9 +10,18 @@ const CartItem = ({ item, handleDelete }) => {
                             <td>{item.product.name}</td>
                             <td>{item.quantity} unidades</td>
                             <td>${item.product.price}</td>
-                            <td>TOTAL: ${item.quantity * item.product.price} </td>
+                            <td>Subtotal: ${item.quantity * item.product.price} </td>
                             <th><button className="btn btn-square btn-sm btn-outline btn-error" onClick={() => handleDelete(item.product.id)}>X</button></th>
                         </tr>
+
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th>TOTAL: {[item.quantity * item.product.price].map(item => <h1>{item}</h1>)}</th>
+                        </tr>
+
                     </tbody>
                 </table>
             </div>
