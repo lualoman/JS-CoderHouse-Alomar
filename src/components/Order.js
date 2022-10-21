b14ee6e2d8144009f2e365562ffe73d774275306
Lucia Alomar <alolu7@gmail.com>
2022-10-21 15:22:05 -0300

    firebase II part2

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,13 @@
 import { addDoc, collection, getFirestore } from 'firebase/firestore'
 //import userOrder from './userOrder'
 import { useState, useContext } from 'react'
-//import { CartContext } from '../context/CartContext'
+import { CartContext } from '../context/CartContext'
 
 const Order = () => {
 
-    //const {items} = useContext(CartContext)
+    const {items} = useContext(CartContext)
     const [user, setUser] = useState({})
-    const items = ['Parlante', 'McBook']// ejemplo, los verdaderos vienen de useContext()
+    //const items = ['Parlante', 'McBook']// ejemplo, los verdaderos vienen de useContext()
 
     const sendOrder = () => {
         //const user = { name: 'Lucia', phone: '234212345', email: 'lu@gmail.com' }
@@ -29,6 +29,7 @@ const Order = () => {
     return (
         <>
             <h1>Compra Final</h1>
+            <userOrder setUser={setUser} />
             {items.map((item, i) => <li key={i}>{item}</li>)}
             <button className="btn" onClick={sendOrder}>Finalizar</button>
         </>
